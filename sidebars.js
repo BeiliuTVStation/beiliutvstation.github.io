164c16dcb7480583cdef0bf167e37a05abfc5260
redhat123456 <1907065810@qq.com>
2022-09-02 14:10:27 +0800

    afdsaf

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,13 @@
 module.exports = {
   docs: {
     // 相对于 docs directory
-    创新基地简介: ['intro'],
+    北流市电视台简介: ['intro'],
     公告: [
       'contest/contest-02',
       'notice/holiday-practice-1',
       'notice/2019-2020-annual-review',
     ],
-    基地各部门简介: [
+    各新闻部门简介: [
       'introduction/Algorithms',
       'introduction/Software',
       'introduction/information-security',
@@ -19,6 +19,5 @@ module.exports = {
     互联网优质资源: [
       'netsource/README',
     ],
-    附: ['doc1', 'doc2', 'doc3', 'mdx'],
   },
 };
