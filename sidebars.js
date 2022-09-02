b70ad786d0f5b57053e5a8f55a4d4622c728f480
redhat123456 <1907065810@qq.com>
2022-09-02 17:19:06 +0800

    asdgfh

--- sidebars.js
+++ sidebars.js
@@ -3,21 +3,15 @@ module.exports = {
     // 相对于 docs directory
     北流市电视台简介: ['intro'],
     公告: [
-      'contest/contest-02',
       'notice/holiday-practice-1',
       'notice/2019-2020-annual-review',
     ],
-    各新闻部门简介: [
+    各新闻简介: [
       'introduction/Algorithms',
       'introduction/Software',
       'introduction/information-security',
       'introduction/Big-Data'
     ],
-    成长: [
-      'growth/growth'
-    ],
-    互联网优质资源: [
-      'netsource/README',
-    ],
+    
   },
 };
